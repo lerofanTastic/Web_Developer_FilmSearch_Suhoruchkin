85746beae57100c6cd7ed1d9ef607172d649033a
lerofanTastic <leroty@bk.ru>
2025-06-18 17:13:08 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Web_Developer_FilmSearch_Suhoruchkin",
   server: {
     proxy: {
       "/api": {
