7e110941d51324e06eaa820b7664c25aab82b543
lerofanTastic <leroty@bk.ru>
2025-06-18 19:11:45 +0800

    deploy fourth try

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Web_Developer_FilmSearch_Suhoruchkin/",
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  },
   server: {
     proxy: {
       "/api": {
